09a0e09d281c0ae8f54ab1e14741a94d0fdc0f43
Mehmet <gate3510@gmail.com>
2020-04-03 19:23:14 -0400

    some changes

--- modules/twitter/twitter_module.py
+++ modules/twitter/twitter_module.py
@@ -28,12 +28,7 @@ base_twitter_url = "https://api.twitter.com/1.1/"
 # setup authentication
 oauth = OAuth1(client_key,client_secret,token,token_secret)
 
-username           = sys.argv[1]
-
-try:
-    os.mkdir("%s/Desktop/" % (lol_directory) % os.path.expanduser("~"))
-except:
-    pass
+username           = sys.argv[1] # enter handler
 
 desktop_directory  = "%s/Desktop/" % os.path.expanduser("~")
 
@@ -47,8 +42,6 @@ try:
 except:
     pass
 
-
-
 log = codecs.open("%s%s/twitter.csv" % (desktop_directory,username),"wb",encoding="utf-8")
 log.write("#,Created Timestamp, Text, Longitude, Latitude\r\n")
 
@@ -57,32 +50,26 @@ log.write("#,Created Timestamp, Text, Longitude, Latitude\r\n")
 #
 def get_usernames(user_ids):
 
-    
     url = "https://api.twitter.com/1.1/users/lookup.json?user_id=%s" % (user_ids)
-    
           
     response = requests.get(url,auth=oauth)
     
     time.sleep(3)
     
     if response.status_code == 200:
-        
         result = json.loads(response.content)
         
         usernames = []
         
         for i in result:
             usernames.append("%s,%s" % (i['screen_name'],i['name']))
-        
         return usernames
-    
     return None
 
 #
 # Main Twitter API function for sending requests
 #
 def send_request(screen_name,relationship_type,next_cursor=None):
-
     
     url = "https://api.twitter.com/1.1/%s/ids.json?screen_name=%s&count=5000" % (relationship_type,username)
     
@@ -94,13 +81,9 @@ def send_request(screen_name,relationship_type,next_cursor=None):
     time.sleep(3)
     
     if response.status_code == 200:
-        
         result = json.loads(response.content)
-        
         return result
-    
     return None
-
 
 #
 # Function that contains the logic for paging through results
@@ -117,28 +100,18 @@ def get_all_friends_followers(username,relationship_type):
     if accounts is not None:
         
         account_list.extend(accounts['ids'])
-        
-        print "[*] Downloaded %d of type %s" % (len(account_list),relationship_type)
 
+        print "[*] Downloaded %d of type %s" % (len(account_list),relationship_type)
         
         # while we have a cursor keep downloading friends and followers
         while accounts['next_cursor'] != 0 and accounts['next_cursor'] != -1:
-
             accounts = send_request(username,relationship_type,accounts['next_cursor'])
-            
             if accounts is not None:
-                
                 account_list.extend(accounts['ids'])
                 print "[*] Downloaded %d of type %s" % (len(account_list),relationship_type)
-
             else:
-                
                 break
-    
-     
     return account_list
-
-
 
 #
 # Download Tweets from a user profile
@@ -156,16 +129,10 @@ def download_tweets(screen_name,number_of_tweets,max_id=None):
     response = requests.get(api_url,auth=oauth)
     
     if response.status_code == 200:
-        
         tweets = json.loads(response.content)
-        
         return tweets
-    
     else:
-        
         print "[*] Twitter API FAILED! %d" % response.status_code
-    
-
     return None
 
 #
@@ -203,15 +170,12 @@ def download_all_tweets(username):
         # grab the oldest Tweet
         if len(tweet_list):
             oldest_tweet = tweet_list[-1]
-        
 
     # add the last few Tweets
     full_tweet_list.extend(tweet_list)
         
     # return the full Tweet list
     return full_tweet_list 
-    
-    
 
 full_tweet_list = download_all_tweets(username)
 
@@ -225,7 +189,6 @@ if full_tweet_list is not None:
     print "[*] Processing %d of %d" % (count,len(full_tweet_list))
     
     for tweet in full_tweet_list:
-        
         
         text = tweet['text'].replace("\"","").replace(",","")
         
@@ -243,8 +206,7 @@ if full_tweet_list is not None:
                         
                         fd = open("%s%s/photos/%s" % (desktop_directory,username,file_name),"wb")
                         fd.write(response.content)
-                        fd.close()
-                    
+                        fd.close()  
         
         if tweet["coordinates"] is not None:
             latitude  = tweet["coordinates"]["coordinates"][0]
@@ -256,8 +218,6 @@ if full_tweet_list is not None:
             else:    
                 latitude  = "None"
                 longitude = "None"
-            
-        
         
         message = "%s,%s,%s,%s,%s\r\n" % (tweet['id'],tweet['created_at'],text,latitude,longitude)
         log.write(message)
@@ -269,7 +229,6 @@ print "[*] Done. Now retrieving friends and followers."
 
 friends = get_all_friends_followers(username,"friends")
 followers = get_all_friends_followers(username,"followers")
-
 
 count         = 0
 final_friends = []
@@ -288,19 +247,16 @@ while count < len(friends):
     for friend in friends[count:index]:
         friends_snippet.append(str(friend))
         
-    
     final_friends.extend(get_usernames(",".join(friends_snippet)))
-    
-    
     count += 100
     
 fd = codecs.open("%s%s/friends.csv" % (desktop_directory,username),"wb",encoding="utf-8")
 fd.write("\r\n".join(final_friends))
 fd.close()
 
-
 final_followers = []
 count           = 0
+
 while count < len(followers):
     
     followers_snippet = []
@@ -315,8 +271,6 @@ while count < len(followers):
         
     
     final_followers.extend(get_usernames(",".join(followers_snippet)))
-    
-    
     count += 100
 
 fd = codecs.open("%s%s/followers.csv" % (desktop_directory,username),"wb",encoding="utf-8")
