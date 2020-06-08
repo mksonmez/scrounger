0149c8f63f68d7c52bea0d2fe7e887a8264f932e
eheifetz <eheifetz@hotmail.com>
2020-06-08 18:55:59 -0400

    Added Looping functionality to Scrounger. Now Generates Report At End

--- scrounger.sh
+++ scrounger.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-value= default
-handler= default
+value="default"
+handler="default"
+DONE="false"
 
 cat ./banner
 
@@ -27,14 +28,37 @@ run_tiktok() {
     read handler
 }
 
-echo " "
-echo "Please input one of the following values"
-echo "Twitter = t"
-echo "Facebook = f"
-echo "Tik Tok (User Info) = c"
-echo "Tik Tok (Download Video) = cd" 
+read_choices() {
+    echo " "
+    echo "Please input one of the following values"
+    echo "Twitter = t"
+    echo "Facebook = f"
+    echo "Tik Tok (User Info) = c"
+    echo "Tik Tok (Download Video) = cd" 
+}
 
+is_done() {
+    echo ""
+    echo "Would you like to run another OSINT Scan? (y/n)" 
+    DONE2="false"
+    while [ $DONE2 == "false" ];
+    do
+        read input
 
+        case $input in
+        y)
+            read_choices
+            DONE2="true"
+            ;;
+        n)
+            DONE="true"
+            DONE2="true"
+            ;;
+        *)
+            echo "Invalid Parameter, Please Input y or n"
+    esac
+    done
+}
 ###-------------------------------------------------------###
 ###    Checks to make sure all the output directories are ###
 ###       created, if they aren't it will make them       ### 
@@ -48,44 +72,52 @@ mkdir -p "/home/osint/Desktop/OSINT_OUTPUT/TikTok"
 ###-------------------------------------------------------###
 ###    Decides which social media module will be opened   ###
 ###-------------------------------------------------------###
-read value
+read_choices
 
+while [ "$DONE" == "false" ];
+do
+    read value
 
-case $value in
-t)
-    run_twitter
-    python ./modules/twitter/twitter_module.py $handler
-    exit 0
-    ;;
-f)
-    run_fb
-    $(cd /home/osint/Desktop/scrounger/modules/facebook && python3.7 scraper.py)
-    wait
-    $(cd /home/osint/Desktop/scrounger/modules/facebook && cp -R data/. "/home/osint/Desktop/OSINT_OUTPUT/Facebook")
-    wait
-    $(cd /home/osint/Desktop/scrounger/modules/facebook/data && rm -rf *)
-    exit 0
-    ;;
-c)
-    mkdir -p "/home/osint/Desktop/OSINT_OUTPUT/TikTok/UserProfiles"
-    run_tiktok
-    python3.7 ./modules/tiktok_user_info/tiktok_module.py --username "$handler"
-    wait
-    cp -R "@$handler" "/home/osint/Desktop/OSINT_OUTPUT/TikTok/UserProfiles"
-    wait
-    rm -rf "@$handler"
-    exit 0
-    ;;
-cd)
-    python3.7 ./modules/tiktok/run.py
-    wait
-    cp -R "videos/" "/home/osint/Desktop/OSINT_OUTPUT/TikTok"
-    wait
-    rm -rf "videos/"
-    exit 0
-    ;;
-*)
-    echo "Invalid Parameter"
-    exit 0
-    ;;
-esac
+    case $value in
+    t)
+        run_twitter
+        python ./modules/twitter/twitter_module.py $handler
+        is_done
+        ;;
+    f)
+        run_fb
+        $(cd /home/osint/Desktop/scrounger/modules/facebook && python3.7 scraper.py)
+        wait
+        $(cd /home/osint/Desktop/scrounger/modules/facebook && cp -R data/. "/home/osint/Desktop/OSINT_OUTPUT/Facebook")
+        wait
+        $(cd /home/osint/Desktop/scrounger/modules/facebook/data && rm -rf *)
+        is_done
+        ;;
+    c)
+        mkdir -p "/home/osint/Desktop/OSINT_OUTPUT/TikTok/UserProfiles"
+        run_tiktok
+        python3.7 ./modules/tiktok_user_info/tiktok_module.py --username "$handler"
+        wait
+        cp -R "@$handler" "/home/osint/Desktop/OSINT_OUTPUT/TikTok/UserProfiles"
+        wait
+        rm -rf "@$handler"
+        is_done
+        ;;
+    cd)
+        python3.7 ./modules/tiktok/run.py
+        wait
+        cp -R "videos/" "/home/osint/Desktop/OSINT_OUTPUT/TikTok"
+        wait
+        rm -rf "videos/"
+        is_done
+        ;;
+    *)
+        echo "Invalid Parameter"
+        is_done
+        ;;
+    esac
+done
+
+./generate_report.sh
+
+exit 0
